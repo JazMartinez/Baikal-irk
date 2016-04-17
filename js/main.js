5a02dcbedc2b97921ea5e23b77b0b61f0a247b93
JazMartinez <Ermolaev_94@mail.ru>
2016-04-18 01:14:10 +0800

    Ready files

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$( document ).ready(function( $ ) {
+    $( '#bl-single-slider' ).sliderPro({
+        width: 1170,
+        height: 450,
+        arrows: true,
+        buttons: false,
+        waitForLayers: true,
+        thumbnailWidth: 163,
+        thumbnailHeight: 100,
+        thumbnailPointer: true,
+        thumbnailsPosition: 'right',
+        autoplay: false,
+        autoScaleLayers: false,
+        /*breakpoints: {
+            500: {
+                thumbnailWidth: 120,
+                thumbnailHeight: 50
+            }
+        }*/
+    });
+});
+
+$( document ).ready(function( $ ) {
+    $( '#bl-attraction-slider' ).sliderPro({
+        width: 1170,
+        height: 450,
+        arrows: true,
+        buttons: false,
+        waitForLayers: true,
+        thumbnailWidth: 310,
+        thumbnailHeight: 200,
+        thumbnailPointer: true,
+        thumbnailArrows: true,
+        thumbnailsPosition: 'right',
+        autoplay: false,
+        autoScaleLayers: false,
+        /*breakpoints: {
+         500: {
+         thumbnailWidth: 120,
+         thumbnailHeight: 50
+         }
+         }*/
+    });
+});
